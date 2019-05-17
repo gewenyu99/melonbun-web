4b9b11b59b12a85a8a43bd02cc0cf89f9a0e66f0
Steven Steven <s3@uwaterloo.ca>
2019-05-17 17:34:52 -0400

    - Edit now changes the redux store (will change database and call 'updateRequestEntry') - Page will load from redux store (during CSR), or do a get request (during SSR)

--- redux/reducers/requestReducer.tsx
+++ redux/reducers/requestReducer.tsx
@@ -1,26 +1,36 @@
-import {IRequestInfo} from '../dataTypes/request'
+import { IRequestInfo } from '../dataTypes/request'
 
-interface IRequestState{
-    requestBuffer:Array<IRequestInfo>
+interface IRequestState {
+    requestBuffer: Array<IRequestInfo>
 }
 
-const initState:IRequestState ={
-    requestBuffer:[]
+const initState: IRequestState = {
+    requestBuffer: []
 }
 
 const requestReducer = (state = initState, action) => {
 
-    let newState = {...state};
-    switch(action.type) {
-        
+    let newState = { ...state };
+    switch (action.type) {
+
         case 'INITIALIZE_ALL_REQUESTS':
-            const {requestList} = action.payload
+            const { requestList } = action.payload
             newState.requestBuffer = requestList ? requestList : initState.requestBuffer;
             break;
 
+        case 'UPDATE_REQUEST_ENTRY':
+            const { id, requestObj } = action.payload
+            // update request
+            newState.requestBuffer = state.requestBuffer.map(request => {
+                if (request.id === id) {
+                    return { ...requestObj }
+                }
+                return request
+            })
+
         default:
-          // code block
-      }
+        // code block
+    }
     return newState;
 }
 
